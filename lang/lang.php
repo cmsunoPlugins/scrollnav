4472a83538e60a135b4fc798f89e2634124ac591
Jacques Malgrange <jacques.malgrange@gmail.com>
2017-03-15 19:41:20 +0100

    V1.2.1

--- lang/lang.php
+++ lang/lang.php
@@ -1,17 +1,20 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
-	$langPlug = array(
-		"fr" => "fr_FR.utf8",
-		"en" => "en_US",
-		"es" => "es_ES.utf8"
-		);
-	//	
-	if(isset($langPlug[$lang]) && $langPlug[$lang])
-		{
-		require_once(dirname(__FILE__).'/../../../includes/lang/php-gettext/gettext.inc');
-		T_setlocale(LC_MESSAGES, $langPlug[$lang]);
-		T_bindtextdomain("scrollnav", dirname(__FILE__));
-		T_bind_textdomain_codeset("scrollnav", "UTF-8");
-		T_textdomain("scrollnav");
-		}
+$langPlug = array(
+	"fr" => "fr_FR.utf8",
+	"en" => "en_US",
+	"es" => "es_ES.utf8"
+	);
+//	
+if(!empty($langPlug[$lang]))
+	{
+	require_once(dirname(__FILE__).'/../../../includes/lang/php-gettext/gettext.inc');
+	T_setlocale(LC_MESSAGES, $langPlug[$lang]);
+	T_bindtextdomain("scrollnav", dirname(__FILE__));
+	T_bind_textdomain_codeset("scrollnav", "UTF-8");
+	T_textdomain("scrollnav");
+	}
+else if(!function_exists('T_'))
+	{
+	function T_($f) { return $f; }
+	}
 ?>
